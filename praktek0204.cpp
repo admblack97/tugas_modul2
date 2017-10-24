d6c303b30fe88b8a8d6d730f266d5d7b0b84960b
admblack97 <admblack97@gmail.com>
2017-10-24 21:47:58 +0800

    rumus perhitungan luas parket

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main () {
 	int LR=42;
+	double LuasParket = 193*1215;
+	// perhitungan luas parket panjang * lebar
+	// 193 * 1215 = 234495
 	cout<<"Luas Rumah : "<<LR<<" m2"<<endl;
-	
+
+	 	
 	 
 }
