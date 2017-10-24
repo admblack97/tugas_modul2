6e94cba8182657b66844f092ad2db8570cb07e27
admblack97 <admblack97@gmail.com>
2017-10-24 21:56:40 +0800

    perhitungan luas 1 box parket

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -7,6 +7,10 @@ int main () {
 	double LuasParket = 193*1215;
 	// perhitungan luas parket panjang * lebar
 	// 193 * 1215 = 234495
+	double 1Box = LuasPaket*8/1000000;
+	// setelah dapat hasil luas parket, selanjut nya
+	// dikalikan 8 keping parket dan dibagi 1000000
+	// untuk mendapatkan hasil berkoma 
 	cout<<"Luas Rumah : "<<LR<<" m2"<<endl;
 
 	 	
