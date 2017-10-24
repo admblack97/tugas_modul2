1013967e2e67822742d8208188366477aa9fa780
admblack97 <admblack97@gmail.com>
2017-10-24 22:03:15 +0800

    sdh dibetulkan, angka td di depan variable

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -8,12 +8,12 @@ int main () {
 	double LuasParket = 193*1215;
 	// perhitungan luas parket panjang * lebar
 	// 193 * 1215 = 234495
-	double 1Box = LuasPaket*8/1000000;
+	double Box = LuasParket*8/1000000;
 	// setelah dapat hasil luas parket, selanjut nya
 	// dikalikan 8 keping parket dan dibagi 1000000
 	// untuk mendapatkan hasil berkoma 
 	cout<<"Luas Rumah        : "<<LR<<" m2"<<endl;
-	cout<<"Luas 1 Box Parket : "<<1Box<<" m2"<<endl;
+	cout<<"Luas 1 Box Parket : "<<Box<<" m2"<<endl;
 
 	 	
 	 
