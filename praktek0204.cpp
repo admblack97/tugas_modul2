f02d3ea435d1e7de60e3671ad6f7480e681da7ca
admblack97 <admblack97@gmail.com>
2017-10-24 22:12:53 +0800

    perhitungan jumlah box parket berhasil

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -12,9 +12,15 @@ int main () {
 	// setelah dapat hasil luas parket, selanjut nya
 	// dikalikan 8 keping parket dan dibagi 1000000
 	// untuk mendapatkan hasil berkoma 
+	double JumlahBox, Parket, Pasang, Total;
 	cout<<"Luas Rumah        : "<<LR<<" m2"<<endl;
 	cout<<"Luas 1 Box Parket : "<<Box<<" m2"<<endl;
-
+	
+	JumlahBox = LR/Box;
+	// untuk mencari jumlah box yg di beli, luas rumah dikali dengan hasil luas 1 box parket
+	cout<<"Jumlah Box Parket : "<<JumlahBox<<endl;
+	
+	
 	 	
 	 
 }
